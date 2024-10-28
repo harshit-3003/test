ec964f7d34d71ca10b8e18057ef93fa7374d53a2
harshit-3003 <guptaharshit00002@gmail.com>
2024-10-28 13:33:24 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,18 @@
+
+#area of rectangle
+a=int(input("enter a no :"))
+b=int(input("enter no. b:"))
+
+print("area of rectangle is:",a*b)
+#AREA OF SQAURE
+print("area of square is :",a*a)
+#AREA OF CIRCLE
+print("area of circle is :",3.14*a*a)
+print("area of circle is :",3.14*b*b)
+#AREA OF TRIANGLE
+c=int(input("enter the height :"))
+d=int(input("enter the base :"))
+print("area of triangle is  :",(c*d)/2)
+
+
+
